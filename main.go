b7e6dedd74e95a7d57f185d113823194c6f57bc7
ambitechstrous <josemvargas94@gmail.com>
2026-08-13 12:58:04 -0400

    Fix audio bugs with OSX

--- main.go
+++ main.go
@@ -1,4 +1,40 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/ambitechstrous/hummingbird/midi"
+)
+
 func main() {
+	// FIXME: Virtual port should be in config instead of hardcoded
+	handler, err := midi.NewMidiHandler(false)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := handler.Close(); err != nil {
+			log.Printf("Error closing MIDI handler: %v", err)
+		}
+	}()
+
+	// Example usage: Send a note (e.g., middle C, which is MIDI note number 60)
+	if err := handler.SendNote(60); err != nil {
+		log.Printf("Error sending note: %v", err)
+	}
+
+	time.Sleep(2 * time.Second) // Hold the note for 2 seconds
+	if err := handler.SendNote(0); err != nil {
+		log.Printf("Error sending note off: %v", err)
+	}
+
+	log.Println("Press Ctrl+C to exit...")
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	<-sig
 }
--- midi/midi.go
+++ midi/midi.go
@@ -1,40 +1,72 @@
 package midi
 
 import (
+	"fmt"
 	"sync"
 
 	gomidi "gitlab.com/gomidi/midi/v2"
-	"gitlab.com/gomidi/midi/v2/drivers"
-	"gitlab.com/gomidi/midi/v2/drivers/rtmididrv"
+	"gitlab.com/gomidi/midi/v2/drivers/rtmididrv/imported/rtmidi"
 )
 
 var _ = gomidi.NoteOn
 
 type MidiHandler struct {
-	midiOut  drivers.Out
+	midiOut  rtmidi.MIDIOut
 	mutex    *sync.Mutex
 	lastNote uint8
 }
 
 type IMidiHandler interface {
-	SendNote(note uint8)
+	SendNote(note uint8) error
+	Close() error
 }
 
-func NewMidiHandler() (IMidiHandler, error) {
-	drv, err := rtmididrv.New()
+func findPortByName(midiOut rtmidi.MIDIOut, name string) (int, error) {
+	portCount, err := midiOut.PortCount()
+	if err != nil {
+		return -1, err
+	} else if portCount == 0 {
+		return -1, fmt.Errorf("no MIDI output ports available")
+	}
+
+	for i := 0; i < portCount; i++ {
+		portName, err := midiOut.PortName(i)
+		if err != nil {
+			return -1, err
+		} else if portName == name {
+			return i, nil
+		}
+	}
+
+	return -1, fmt.Errorf("port not found: %s", name)
+}
+
+func NewMidiHandler(useVirtualPort bool) (IMidiHandler, error) {
+	midiOut, err := rtmidi.NewMIDIOutDefault()
 	if err != nil {
 		return nil, err
 	}
 
-	out, err := drv.OpenVirtualOut("Hummingbird")
-	if err != nil {
-		return nil, err
+	if useVirtualPort {
+		if err = midiOut.OpenVirtualPort("Hummingbird"); err != nil {
+			return nil, err
+		}
+	} else {
+		// FIXME: Make port name configurable or infer via machine config
+		targetPort, err := findPortByName(midiOut, "IAC Driver Bus 1")
+		if err != nil {
+			return nil, err
+		}
+
+		if err = midiOut.OpenPort(targetPort, "Hummingbird"); err != nil {
+			return nil, err
+		}
 	}
 
-	return &MidiHandler{midiOut: out, lastNote: 0, mutex: &sync.Mutex{}}, nil
+	return &MidiHandler{midiOut: midiOut, lastNote: 0, mutex: &sync.Mutex{}}, nil
 }
 
-func (h *MidiHandler) SendNote(note uint8) {
+func (h *MidiHandler) SendNote(note uint8) error {
 	// Lock mutex to ensure notes are processed sequentially
 	h.mutex.Lock()
 	defer h.mutex.Unlock()
@@ -43,17 +75,23 @@ func (h *MidiHandler) SendNote(note uint8) {
 	if h.lastNote != note {
 		// Send Note Off if the last note was not silence (i.e. non-zero)
 		if h.lastNote != 0 {
-			noteOff := []byte{0, h.lastNote, 0} // Channel 0, velocity 0
-			h.midiOut.Send(noteOff)
+			noteOff := gomidi.NoteOff(0, h.lastNote)
+			return h.midiOut.SendMessage(noteOff)
 		}
 
 		// So long as the note isn't silence (i.e. non-zero), send the new note
 		if note != 0 {
-			noteOn := []byte{0, note, 100} // Channel 0, velocity 100
-			h.midiOut.Send(noteOn)
+			noteOn := gomidi.NoteOn(0, note, 100) // Channel 0, velocity 100
+			return h.midiOut.SendMessage(noteOn)
 		}
 
 		// Update the last note played to the current note
 		h.lastNote = note
 	}
+
+	return nil
+}
+
+func (h *MidiHandler) Close() error {
+	return h.midiOut.Close()
 }
